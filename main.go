6bca962c4e98baf05478ff89f0976f10428672f3
cloudian <nguyenkhaan2006@gmail.com>
2026-08-19 01:03:22 +0700

    feat: demo simple ai chatbot response

--- main.go
+++ main.go
@@ -1,9 +1,75 @@
+// https://stackoverflow.com/questions/67248551/go-get-values-from-env-file
+
 package main
 
 import (
-	"cloudian/cloudian-claw/cmd/agentkit"
-)
+	"bufio"
+	"context"
+	"fmt"
+	"log"
+	"os"
+	"strings"
 
+	"github.com/joho/godotenv"
+	"github.com/openai/openai-go/v3"
+	"github.com/openai/openai-go/v3/option"
+)
 func main() {
-	agentkit.Execute() 
+	err := godotenv.Load() 
+	if err != nil {
+		fmt.Println("Error while loading .env")
+		os.Exit(1) 
+	}
+	apiKey := os.Getenv("OPENROUTER_API_KEY")
+	if apiKey == "" {
+		log.Fatal("API key is required")
+		os.Exit(1)
+	}
+	url := "https://openrouter.ai/api/v1"
+
+	client := openai.NewClient(
+		option.WithBaseURL(url),  
+		option.WithAPIKey(apiKey), 
+	)
+	messages := []openai.ChatCompletionMessageParamUnion{}
+	model := "z-ai/glm-5.2:free"
+	ctx := context.Background() //Khong cho dung cong bviec giua chung, context.Background() noi rang cong viec nay la vo han thoi gian 
+	//hay thuc hien cong viec nay cho den khi nao hoan thanh xong cong viec 
+	params := openai.ChatCompletionNewParams{
+		Model: model, 
+		Messages: messages,
+	}
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print("> ") 
+		if !scanner.Scan() {
+			break 
+		}
+		//Su dung goi thu vien strings de xu ly chuoi 
+		input := strings.TrimSpace(
+			scanner.Text(), 
+		)
+		if input == "" {
+			continue
+		}
+		if input == "clear" {
+			//Clear the terminal = Print the spcial key 
+			fmt.Print("\033[H\033[2J")
+			continue 
+		}
+		if input == "exit" {
+			fmt.Println("Goodnight") 
+			break 
+		}
+		//Append the messages to the param's messages 
+		params.Messages = append(params.Messages , openai.UserMessage(input))
+		
+		response, err := client.Chat.Completions.New(ctx , params)
+		if err != nil {
+			log.Fatal(err)
+		} 
+		fmt.Println("[AGENT]: ", response.Choices[0].Message.Content) 
+		//Build the agent loop fuck u :v 
+	}
+
 }
